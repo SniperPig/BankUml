ae202988fa301622e56577639dfa2be3af3a2200
vincentbenesen <vincebry0803@gmail.com>
2025-11-18 20:05:14 -0500

    Fix/Align JavaFX with JDK 17

--- src/main/java/bank/Models/Card.java
+++ src/main/java/bank/Models/Card.java
@@ -1,18 +1,37 @@
-package bank.Models;
+// package bank.Models;
 
-public class Card extends Account {
-    public Card(Customer customer) {
-        super(customer);
-    }
+// import java.time.LocalDateTime;
 
-    @Override
-    public void pay() {
-        System.out.println("Card payment for: " + customer.getName());
-    }
+// public class Card extends Account {
 
-    @Override
-    public void receipt() {
-        System.out.println("Card receipt for: " + customer.getName());
-    }
-}
+//     public Card(int accountId,
+//                 int customerId,
+//                 int branchId,
+//                 String accountNumber,
+//                 double balance,
+//                 double interestRate,
+//                 String bankCode,
+//                 LocalDateTime createdAt) {
 
+//         super(accountId,
+//               customerId,
+//               branchId,
+//               accountNumber,
+//               "CARD",
+//               balance,
+//               interestRate,
+//               null,
+//               bankCode,
+//               createdAt);
+//     }
+
+//     @Override
+//     public void pay() {
+//         System.out.println("Card payment processed for account " + accountNumber + ".");
+//     }
+
+//     @Override
+//     public void receipt() {
+//         System.out.println("Card receipt generated for account " + accountNumber + ".");
+//     }
+// }
