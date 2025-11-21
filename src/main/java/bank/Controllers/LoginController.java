c586b77c153cc0c7ce0204b09397a873cfb179c5
LisaHanna13 <lisa-hanna@hotmail.com>
2025-11-21 17:48:58 -0500

    Finished designing the GUI for LoginForm

---
+++ src/main/java/bank/Controllers/LoginController.java
@@ -0,0 +1,13 @@
+package bank.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+
+public class LoginController {
+}
